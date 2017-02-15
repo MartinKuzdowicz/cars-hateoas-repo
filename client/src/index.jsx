084535b15a33e29d4eaf268ed31bc80d804f2f09
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-02-15 23:08:23 +0000

    basic pagination implementation

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,19 +6,48 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {cars: ''};
+        this.state = {
+            cars: '', links: [{
+                rel: '',
+                href: ''
+            }],
+            carsUrl: 'http://localhost:3000/api/cars-repo/cars'
+        };
+    }
+
+    handleNextBtnClick(links) {
+        const apiUrl = links
+            .filter((l) => l.rel == 'next')
+            .map((l) => l.href);
+
+        this.setState({
+            carsUrl: apiUrl
+        });
+        this.componentDidMount();
+    }
+
+    handlePrevBtnClick(links) {
+        const apiUrl = links
+            .filter((l) => l.rel == 'prev')
+            .map((l) => l.href);
+
+        this.setState({
+            carsUrl: apiUrl
+        });
+        this.componentDidMount();
     }
 
     componentDidMount() {
         var component = this;
-        axios.get('http://localhost:3000/api/cars-repo/cars').then((res) => {
-            const carsArr = res.data;
+        axios.get(this.state.carsUrl).then((res) => {
+            console.log(res.data.links);
+            const carsArr = res.data.content;
             const dataAsLiElmts = carsArr.map((car) => {
                 return (
                     <li key={car._id}>{car.name}</li>
                 );
             });
-            component.setState({cars: dataAsLiElmts});
+            component.setState({cars: dataAsLiElmts, links: res.data.links});
         });
 
     }
@@ -28,6 +57,13 @@ class App extends React.Component {
             <div>
                 <h1>Cars repository UI</h1>
                 <ul>{this.state.cars}</ul>
+                <hr />
+                <button onClick={() => this.handleNextBtnClick(this.state.links)}>
+                    next
+                </button>
+                <button onClick={() => this.handlePrevBtnClick(this.state.links)}>
+                    prev
+                </button>
             </div>
         );
     }
